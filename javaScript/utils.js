82e082e3bba523de2bad7b06395e44875f0f8980
Belen Ramos <m.b.ramos0204@gmail.com>
2024-08-03 16:11:27 -0300

    Arreglo de advertencias

--- javaScript/utils.js
+++ javaScript/utils.js
@@ -1,20 +1,16 @@
-// function mostrarMensaje(mensaje) {
-//     var messageContainer = document.getElementById('message-container');
-//     messageContainer.textContent = mensaje;
-// }
-
 // utils.js
 function mostrarMensaje(mensaje) {
     var modal = document.getElementById('message-modal');
     var modalMessage = document.getElementById('modal-message');
-    var closeButton = document.querySelector('.close-button');
 
     modalMessage.textContent = mensaje;
-    modal.style.display = 'block'; // Muestra el modal
+    modal.style.display = 'block'; 
 
-    closeButton.addEventListener('click', function() {
+    // Configura el tiempo para cerrar el modal automáticamente 
+    var tiempoAutomatico = 2000; // 2 segundos
+    setTimeout(function() {
         modal.style.display = 'none'; // Oculta el modal
-    });
+    }, tiempoAutomatico);
 
     // Cierra el modal si se hace clic fuera del contenido del modal
     window.addEventListener('click', function(event) {
@@ -23,4 +19,3 @@ function mostrarMensaje(mensaje) {
         }
     });
 }
-
